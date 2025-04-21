51c15803251d264e2658b4c6e4df04492aafac7c
Daniel Boesswetter <daniel@daniel-boesswetter.de>
2025-04-21 16:05:47 +0200

    adding electricitymap crawler

---
+++ nadiki-facility-electricitymap-crawler.py
@@ -0,0 +1,28 @@
+# Partly copied from Robin Pesl's code
+import os
+import requests
+import dateutil.parser
+import pprint
+
+def fetch_electricity_data() -> list:
+    url = "https://api.electricitymap.org/v3/carbon-intensity/history?zone=NL" # FIXME: take the zone from an env variable but convert it to two letters before
+    response = requests.get(url, headers={"auth-token": os.environ.get("ELECTRICITYMAP_AUTH_TOKEN")})
+    response.raise_for_status()
+    data = response.json()
+    history = data.get("history", [])
+    return history
+#    history = [
+#        {
+#            "value": float(item["carbonIntensity"]),
+#            "time": dateutil.parser.isoparse(item["datetime"]),
+#        }
+#        for item in history
+#    ]
+#    return history
+
+def print_em_history_as_influx_data(history):
+    for item in history:
+        print(f"facility,country_code={os.environ.get('TAG_COUNTRY_CODE')},facility_id={os.environ.get('TAG_FACILITY_ID')} grid_emission_factor_grams={item['carbonIntensity']} {int(dateutil.parser.parse(item['datetime']).timestamp())*10**9}")
+
+if __name__ == "__main__":
+    print_em_history_as_influx_data(fetch_electricity_data())
