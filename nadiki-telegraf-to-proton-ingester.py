151afeebc5fc50b1d8897d4413680ca321e930c4
Daniel Boesswetter <daniel@daniel-boesswetter.de>
2025-05-29 08:12:34 +0200

    add querying of Proton and pass back results to Telegraf

--- nadiki-telegraf-to-proton-ingester.py
+++ nadiki-telegraf-to-proton-ingester.py
@@ -10,6 +10,11 @@ import shlex
 import signal
 import json
 import sys
+import requests
+import os
+from proton_driver import client
+
+PROTON_INGEST_URL = "http://localhost:3218/proton/v1/ingest/streams/influxdb_metrics"
 
 def parse_line(line):
     # parse the input line (Influx line protocol)
@@ -34,13 +39,31 @@ def dump_metrics(a,b):
     global data
     print("alarm!", file=sys.stderr)
     if len(data) > 0:
-        print(json.dumps({ "columns": columns, "data": data }), file=sys.stderr)
+        #print(json.dumps({ "columns": columns, "data": data }), file=sys.stderr)
+        headers = {"Content-Type": "application/json"}
+        payload = { "columns": columns, "data": data }
+        response = requests.post(PROTON_INGEST_URL, headers=headers, json=payload)
         data = []
     signal.alarm(1)
 
-signal.signal(signal.SIGALRM, dump_metrics)
-signal.alarm(1)
-
 if __name__ == "__main__":
-    for line in fileinput.input():
-        data.append(parse_line(line))
+    # we should fork one process per query
+    if os.fork() > 0:
+        # child process queries proton and outputs metrics
+        c = client.Client(host='127.0.0.1', port=8463)
+        rows = c.execute_iter(
+            "SELECT * FROM influxdb_metrics"
+            #"SELECT server, map_cast(['power_consumption'], [fields['current_power_consumption_watts']*3600/(timestamp-lag(timestamp))]) FROM influxdb_metrics WHERE fields['current_power_consumption_watts'] != ''"
+        )
+        for row in rows:
+            (measurement, tags, fields, timestamp, ts) = row
+            tag_strings = [f"{tagname}={tags[tagname]}" for tagname in tags.keys()]
+            field_strings = [f"{fieldname}={fields[fieldname]}" for fieldname in fields.keys()]
+            print(f"{measurement},{','.join(tag_strings)} {'.'.join(field_strings)} {timestamp}")
+            #print(row, file=sys.stderr)
+    else:
+        # parent process does the ingestion into proton
+        signal.signal(signal.SIGALRM, dump_metrics)
+        signal.alarm(1)
+        for line in fileinput.input():
+            data.append(parse_line(line))
