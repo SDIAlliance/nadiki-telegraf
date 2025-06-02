f8b6b4ef8b66a970edd54a7d9aee52711603efaa
Daniel Boesswetter <daniel@daniel-boesswetter.de>
2025-06-02 08:07:12 +0200

    working victoriametrics crawler based on their export

--- nadiki-victoriametrics-crawler.py
+++ nadiki-victoriametrics-crawler.py
@@ -1,8 +1,15 @@
 import requests
 from requests.auth import HTTPBasicAuth
 import os
+import sys
+import time
+import signal
+import json
+import pprint
 
 SOCKS_PROXY=os.environ.get("SOCKS_PROXY")
+
+last_timestamp = dict()
 
 class VMQuery:
 
@@ -10,15 +17,37 @@ class VMQuery:
         self.url = url
     
     def query(self, metricname):
-        response = requests.get(f"{self.url}/select/0/prometheus/api/v1/query?query={metricname}", proxies=dict(http=SOCKS_PROXY, https=SOCKS_PROXY))
+        #response = requests.get(f"{self.url}/select/0/prometheus/api/v1/query?query={metricname}", proxies=dict(http=SOCKS_PROXY, https=SOCKS_PROXY))
+        # We use the VictoriaMetrics expor instead of the query endpoint because we need stable timestamps without interpolation:
+        response = requests.get(f"{self.url}/select/0/prometheus/api/v1/export?match[]={metricname}&start=-1h", proxies=dict(http=SOCKS_PROXY, https=SOCKS_PROXY))
         response.raise_for_status()
-        return response.json()
+        return response.text
 
-if __name__ == "__main__":
+def signal_handler(a,b):
+#    pprint.pprint(last_timestamp)
     vmq = VMQuery(os.environ.get("VICTORIA_METRICS_URL"))
     r = vmq.query(os.environ.get("VICTORIA_METRICS_METRIC"))
-    for x in r["data"]["result"]:
-        tag_string = ",".join([f"{k}={x['metric'][k]}" for k in x["metric"]])
-        field_string = f"value={x['value'][1]}"
-        timestamp = x["value"][0]
+    for line in r.split("\n"):
+        if not line.strip(): # ignore empty lines
+            continue
+        data_point = json.loads(line)
+        timestamp = data_point["timestamps"][0]
+#        # only consider lines newer than the last one seen
+#        try:
+#            last_ts = last_timestamp[f'{data_point["metric"]["__name__"]}/{data_point["metric"]["instance"]}']
+#            if last_ts >= timestamp:
+#                continue
+#        except:
+#            pass
+        tag_string = ",".join([f"{k}={data_point['metric'][k]}" for k in data_point["metric"]])
+        field_string = f"value={data_point['values'][0]}"
+        last_timestamp[f'{data_point["metric"]["__name__"]}/{data_point["metric"]["instance"]}'] = timestamp
         print(f"{os.environ.get('VICTORIA_METRICS_METRIC')},{tag_string} {field_string} {timestamp}000000")
+
+def main():
+    signal.signal(signal.SIGHUP, signal_handler)
+    while True:
+        time.sleep(60)
+
+if __name__ == "__main__":
+    main()
